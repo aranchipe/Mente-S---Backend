ae80bd982fe9783f4c3613fa78964785d50c199d
Lucas <aranchipe98@gmail.com>
2022-09-08 22:35:03 -0300

    crud de pacientes finalizado

--- src/controllers/pacientes.js
+++ src/controllers/pacientes.js
@@ -99,7 +99,128 @@ const listarPacientes = async (req, res) => {
     }
 }
 
+const detalharPaciente = async (req, res) => {
+    const { id } = req.params
+    const { profissional } = req
+
+
+    try {
+        const paciente = await knex('pacientes').where({ id }).andWhere({ profissional_id: profissional.id }).first()
+        console.log(paciente)
+
+        if (!paciente) {
+            return res.status(404).json({ "mensagem": "Paciente não encontrado" })
+        }
+
+        return res.status(200).json(paciente)
+    } catch (error) {
+        return res.status(500).json({ "mensagem": error.message })
+    }
+}
+
+const atualizarPaciente = async (req, res) => {
+    const { id } = req.params
+    const { nome, data_nascimento, cpf, genero, endereco, email, telefone } = req.body
+    const { profissional } = req
+
+
+    try {
+        schemaCadastroPaciente.validate(req.body)
+
+        const pacienteEncontrado = await knex('pacientes')
+            .where({ id })
+            .andWhere({ profissional_id: profissional.id })
+
+        if (pacienteEncontrado.length === 0) {
+            return res.status(404).json({ "mensagem": "Paciente não encontrado" })
+        }
+
+        if (cpf.length !== 11) {
+            return res.status(400).json({ "mensagem": "Digite um cpf válido" })
+        }
+
+        const emailPacienteEncontrado = await knex('pacientes')
+            .where({ email })
+            .andWhere('id', '!=', id)
+            .first();
+
+        if (emailPacienteEncontrado) {
+            return res.status(400).json({ "mensagem": "Email já cadastrado" });
+        }
+
+        const cpfPacienteEncontrado = await knex('pacientes')
+            .where({ cpf })
+            .andWhere('id', '!=', id)
+            .first();
+
+
+        if (cpfPacienteEncontrado) {
+            return res.status(400).json({ "mensagem": "CPF já cadastrado" });
+        }
+
+        const telefonePacienteEncontrado = await knex('pacientes')
+            .where({ telefone })
+            .andWhere('id', '!=', id)
+            .first();
+
+        if (telefonePacienteEncontrado) {
+            return res.status(400).json({ "mensagem": "Número de telefone já cadastrado" });
+        }
+
+        const pacienteCadastrado = await knex('pacientes')
+            .update({
+                nome,
+                email,
+                cpf,
+                telefone,
+                endereco,
+                genero,
+                data_nascimento
+            }).where({ id });
+
+        if (pacienteCadastrado.length === 0) {
+            return res.status(500).json({ "mensagem": "Não foi possível cadastrar o paciente" });
+        }
+
+        return res.status(200).json({ "mensagem": "Os dados do paciente foram atualizados com sucesso" });
+
+
+    } catch (error) {
+        return res.status(500).json({ "mensagem": error.message });
+    }
+}
+
+const deletarPaciente = async (req, res) => {
+    const { id } = req.params
+    const { profissional } = req
+
+
+    try {
+        const pacienteEncontrado = await knex('pacientes')
+            .where({ id })
+            .andWhere({ profissional_id: profissional.id })
+
+
+        if (pacienteEncontrado.length === 0) {
+            return res.status(404).json({ "mensagem": "Paciente não encontrado" })
+        }
+        const pacienteExcluido = await knex('pacientes').del().where({ id })
+
+        if (!pacienteExcluido) {
+            return res.status(400).json({ "mensagem": "Não foi possível excluir o paciente" })
+        }
+
+        return res.status(200).json({ "mensagem": "Paciente excluído com sucesso" })
+    } catch (error) {
+        return res.status(500).json({ "mensagem": error.message });
+
+    }
+}
+
 module.exports = {
     cadastroPaciente,
-    listarPacientes
+    listarPacientes,
+    detalharPaciente,
+    atualizarPaciente,
+    deletarPaciente
 }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { cadastroPaciente, listarPacientes } = require('../controllers/pacientes');
+const { cadastroPaciente, listarPacientes, detalharPaciente, deletarPaciente, atualizarPaciente } = require('../controllers/pacientes');
 const { login, cadastroProfissional } = require('../controllers/profissionais');
 const { verificarLogin } = require('../middlewares/verificarLogin');
 
@@ -12,5 +12,8 @@ route.use(verificarLogin)
 
 route.post('/paciente', cadastroPaciente)
 route.get('/paciente', listarPacientes)
+route.get('/paciente/:id', detalharPaciente)
+route.put('/paciente/:id', atualizarPaciente)
+route.delete('/paciente/:id', deletarPaciente)
 
 module.exports = route
